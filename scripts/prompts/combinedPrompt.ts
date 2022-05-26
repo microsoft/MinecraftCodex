a3ac8def7ff7da507641ac9e9d7acd595119720f
Joshua Carlson <61999256+minerj101@users.noreply.github.com>
2022-05-25 22:45:22 -0400

    fixed typo for array

--- scripts/prompts/combinedPrompt.ts
+++ scripts/prompts/combinedPrompt.ts
@@ -12,7 +12,7 @@ export let context: Context = {
 // listInventory(object: Block | SimulatedPlayer | Player): InventoryComponentContainer - returns a container enumerating all the items a player or treasure chest has
 // getLocation() : BlockLocation - returns the location from the simulated player
 // navigateLocation(worldLocation: Location : Block, speed?: number) : Promise<NavigationResult> - path find through the world to a location
-// findBlock(type: string, maxRadius: number, numFind : number = 1): Block [] - Returns the an arry of blocks closest to the simulated player of the type given within the radius.
+// findBlock(type: string, maxRadius: number, numFind : number = 1): Block [] - Returns the an array of blocks closest to the simulated player of the type given within the radius.
 // followEntity(entity: Entity, speed? : number): Promise<void> - Orders the simulated player to move to the given entity.
 // mineBlock(blockArr : Block []) : boolean - simulated player mines a block and places it in their inventory
 // collectNearbyItems() : number - Collects nearby items that may be on the ground - for example, mined ore
